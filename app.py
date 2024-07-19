4339c08ab5cb6257582212fb340875649d5d9f52
PaoloCarmine1201 <114732828+PaoloCarmine1201@users.noreply.github.com>
2024-07-19 15:37:41 +0200

    create db connection

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template
+import service.db_connection as db
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Stringa di connessione a MongoDB Atlas
-uri = "mongodb+srv://paolocarminevalletta:80Gn6EG85vp8A363@dataset.j95khcl.mongodb.net/?retryWrites=true&w=majority&appName=Dataset"
-client = MongoClient(uri)
-
-# Sostituisci 'nome_database' e 'nome_collezione' con i nomi del tuo database e collezione
-db = client['dataflix']
-collection = db['dataflix']
-
 @app.route('/')
 def index():
     # Recupero di tutte le entry dal database
-    entries = list(collection.find())
+    entries = db.connection()
     return render_template('index.html', entries=entries)
 
 if __name__ == '__main__':
---
+++ service/db_connection.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+uri = "mongodb+srv://paolocarminevalletta:80Gn6EG85vp8A363@dataset.j95khcl.mongodb.net/?retryWrites=true&w=majority&appName=Dataset&tlsAllowInvalidCertificates=true"
+client = MongoClient(uri)
+
+db = client['dataflix']
+collection = db['dataflix']
+
+def connection():
+    entries = list(collection.find())
+    return entries
