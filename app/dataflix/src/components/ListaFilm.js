0957c922ad9233f639008cf1aa6cf39ac05b7dcb
alessaless <zoccolaaless@gmail.com>
2024-07-21 11:44:40 +0200

    show first 80 content in home page

--- app/dataflix/src/components/ListaFilm.js
+++ app/dataflix/src/components/ListaFilm.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import CardFilm from "./CardFilm";
 import { Modal, Button } from 'react-bootstrap';
-import './styles/ListaFilm.css'
-
+import './styles/ListaFilm.css';
 
 function ListaFilm() {
     const [films, setFilms] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [years, setYears] = useState([]);
 
     const handleShowModal = (movie) => {
         setSelectedMovie(movie);
@@ -23,7 +23,7 @@ function ListaFilm() {
         // Funzione per fare la fetch dei film con POST
         const fetchFilms = async () => {
             try {
-                const response = await fetch('http://127.0.0.1:5000/movie/findAllMovie', {
+                const response = await fetch('http://127.0.0.1:5000/get_first_80_entries', {
                     method: 'POST', // Cambia il metodo a POST
                     headers: {
                         'Content-Type': 'application/json',
@@ -47,36 +47,78 @@ function ListaFilm() {
         fetchFilms(); // Chiama la funzione per ottenere i film
     }, []); 
 
+    useEffect(() => {
+        const fetchReleaseYears = async () => {
+          try {
+            const response = await fetch('http://127.0.0.1:5000/get_release_years', {
+              method: 'POST', // Metodo POST
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({}), // Corpo della richiesta, se necessario
+            });
+    
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            setYears(data);
+          } catch (error) {
+            console.error('Errore nella fetch degli anni di rilascio:', error);
+          }
+        };
+    
+        fetchReleaseYears(); // Chiama la funzione per ottenere gli anni di rilascio
+    }, []);
 
+    const handleYearChange = async (event) => {
+        const selectedYear = event.target.value;
+        
+        try {
+            const response = await fetch('http://127.0.0.1:5000/find_by_year', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ year: selectedYear }),
+            });
 
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            setFilms(data); // Imposta i film nello stato con i risultati filtrati
+        } catch (error) {
+            console.error('Errore nella fetch dei film per anno:', error);
+        }
+    };
 
     return (
         <div className="row containerCards">
 
             <div className='containerInput mb-3'>
                 <input type="text" className="form-control inputText" id="titleSearch" placeholder="Ricerca per titolo"/> 
-                <input type="text" className="form-control inputText" id="actorsSearch" placeholder="Ricerca per attori"/> 
-                <select class="form-select inputText" aria-label="Default select example">
+                <input type="text" className="form-control inputText" id="actorsSearch" placeholder="Ricerca per attori/regista"/> 
+                <select className="form-select inputText" aria-label="Default select example" onChange={handleYearChange}>
                     <option selected>Anno di rilascio</option>
-                    <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
+                    {years.map(year => (
+                        <option key={year} value={year}>{year}</option>
+                    ))}
                 </select>
-                <select class="form-select inputText" aria-label="Default select example">
+                <select className="form-select inputText" id='annoRilascio' aria-label="Default select example">
                     <option selected>Tipo di contenuto</option>
                     <option value="Movie">Film</option>
                     <option value="TV Show">Serie</option>
                 </select>
-                <button type="button" className="btn btn-primary">Cerca</button>
             </div>
 
-
-            {films.slice(0, 10).map((film, index) => (
+            {films.map((film, index) => (
                 <CardFilm key={index} elemento={film} onShowModal={handleShowModal} />
             ))}
 
-
-            <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal show={showModal} onHide={handleCloseModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header closeButton>
                     <h1 className="display-6">{selectedMovie?.title}</h1>
                 </Modal.Header>
