4aa6352dfb35dafda16d8d84e2b93577d13bd022
PaoloCarmine1201 <114732828+PaoloCarmine1201@users.noreply.github.com>
2024-07-22 12:17:16 +0200

    add fetch for first 80 film and first 80 TV series add change for cardSerie

--- app/dataflix/src/components/CardFilm.js
+++ app/dataflix/src/components/CardFilm.js
@@ -10,10 +10,20 @@ function CardFilm (props) {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const getImageSrc = () => {
+        if (props.elemento.type === 'Movie') {
+            return 'https://www.ceotech.it/wp-content/uploads/2022/05/Netflix-offre-a-pochi-laccesso-anticipato-a-film-e-serie.jpg';
+        } else if (props.elemento.type === 'TV Show') {
+            return 'https://cdn.dday.it/system/uploads/news/main_image/34284/main_netflixclassificamain.jpg';
+        }
+        return 'https://example.com/default-image.jpg';
+    };
+
     return (
         <div className='containerCard col-md-3 col-12'>
             <Card className='col-12'>
-            <Card.Img variant="top" src="https://www.ceotech.it/wp-content/uploads/2022/05/Netflix-offre-a-pochi-laccesso-anticipato-a-film-e-serie.jpg" />
+            <Card.Img variant="top" src={getImageSrc()} />
             <Card.Body>
                 <Card.Title>{props.elemento.title}</Card.Title>
                 <Card.Text>
--- app/dataflix/src/components/ListaFilm.js
+++ app/dataflix/src/components/ListaFilm.js
@@ -4,27 +4,27 @@ import { Modal, Button } from 'react-bootstrap';
 import './styles/ListaFilm.css';
 
 function ListaFilm() {
-    const [films, setFilms] = useState([]);
+    const [contenuti, setContenuti] = useState([]);
     const [showModal, setShowModal] = useState(false);
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [selectedContent, setSelectedContent] = useState(null);
     const [years, setYears] = useState([]);
+    const [selectedType, setSelectedType] = useState('Movie'); // Tipo di contenuto selezionato
 
     const handleShowModal = (movie) => {
-        setSelectedMovie(movie);
+        setSelectedContent(movie);
         setShowModal(true);
     };
 
     const handleCloseModal = () => {
         setShowModal(false);
-        setSelectedMovie(null);
+        setSelectedContent(null);
     };
 
     useEffect(() => {
-        // Funzione per fare la fetch dei film con POST
-        const fetchFilms = async () => {
+        const fetchEntries = async () => {
             try {
                 const response = await fetch('http://127.0.0.1:5000/get_first_80_entries', {
-                    method: 'POST', // Cambia il metodo a POST
+                    method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                     },
@@ -32,49 +32,48 @@ function ListaFilm() {
                     body: JSON.stringify({}),
                 });
 
-                // Controlla se la risposta è ok
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
 
                 const data = await response.json();
-                setFilms(data); // Imposta i film nello stato
+                setContenuti(data);
             } catch (error) {
-                console.error('Errore nella fetch dei film:', error);
+                console.error('Errore nella fetch dei contenuti:', error);
             }
         };
 
-        fetchFilms(); // Chiama la funzione per ottenere i film
-    }, []); 
+        fetchEntries();
+    }, []);
 
     useEffect(() => {
         const fetchReleaseYears = async () => {
-          try {
-            const response = await fetch('http://127.0.0.1:5000/get_release_years', {
-              method: 'POST', // Metodo POST
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({}), // Corpo della richiesta, se necessario
-            });
-    
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
+            try {
+                const response = await fetch('http://127.0.0.1:5000/get_release_years', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({}),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setYears(data);
+            } catch (error) {
+                console.error('Errore nella fetch degli anni di rilascio:', error);
             }
-    
-            const data = await response.json();
-            setYears(data);
-          } catch (error) {
-            console.error('Errore nella fetch degli anni di rilascio:', error);
-          }
         };
-    
-        fetchReleaseYears(); // Chiama la funzione per ottenere gli anni di rilascio
+
+        fetchReleaseYears();
     }, []);
 
     const handleYearChange = async (event) => {
         const selectedYear = event.target.value;
-        
+
         try {
             const response = await fetch('http://127.0.0.1:5000/find_by_year', {
                 method: 'POST',
@@ -89,46 +88,78 @@ function ListaFilm() {
             }
 
             const data = await response.json();
-            setFilms(data); // Imposta i film nello stato con i risultati filtrati
+            setContenuti(data);
         } catch (error) {
-            console.error('Errore nella fetch dei film per anno:', error);
+            console.error('Errore nella fetch dei contenuti per anno:', error);
+        }
+    };
+
+    const handleContentTypeChange = async (event) => {
+        const selectedType = event.target.value;
+        setSelectedType(selectedType);
+
+        let endpoint = '';
+        if (selectedType === 'Movie') {
+            endpoint = 'http://127.0.0.1:5000/get_first_80_movies'; // Endpoint per film
+        } else if (selectedType === 'TV Show') {
+            endpoint = 'http://127.0.0.1:5000/get_first_80_series'; // Endpoint per serie TV
+        }
+
+        if (endpoint) {
+            try {
+                const response = await fetch(endpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({}),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setContenuti(data);
+            } catch (error) {
+                console.error('Errore nella fetch dei contenuti:', error);
+            }
         }
     };
 
     return (
         <div className="row containerCards">
-
             <div className='containerInput mb-3'>
                 <input type="text" className="form-control inputText" id="titleSearch" placeholder="Ricerca per titolo"/> 
                 <input type="text" className="form-control inputText" id="actorsSearch" placeholder="Ricerca per attori/regista"/> 
                 <select className="form-select inputText" aria-label="Default select example" onChange={handleYearChange}>
-                    <option selected>Anno di rilascio</option>
+                    <option value="">Anno di rilascio</option>
                     {years.map(year => (
                         <option key={year} value={year}>{year}</option>
                     ))}
                 </select>
-                <select className="form-select inputText" id='annoRilascio' aria-label="Default select example">
-                    <option selected>Tipo di contenuto</option>
+                <select className="form-select inputText" id='annoRilascio' aria-label="Default select example" onChange={handleContentTypeChange} defaultValue="Tipo di contenuto">
+                    <option disabled>Tipo di contenuto</option>
                     <option value="Movie">Film</option>
                     <option value="TV Show">Serie</option>
                 </select>
             </div>
 
-            {films.map((film, index) => (
-                <CardFilm key={index} elemento={film} onShowModal={handleShowModal} />
+            {contenuti.map((contenuto, index) => (
+                <CardFilm key={index} elemento={contenuto} onShowModal={handleShowModal} />
             ))}
 
             <Modal show={showModal} onHide={handleCloseModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header closeButton>
-                    <h1 className="display-6">{selectedMovie?.title}</h1>
+                    <h1 className="display-6">{selectedContent?.title}</h1>
                 </Modal.Header>
                 <Modal.Body>
-                    <span>{selectedMovie?.description}</span> <br/><br/>
-                    <span><span className='grassetto'>Release Date: </span>{selectedMovie?.release_year}</span> <br/><br/>
-                    <span><span className='grassetto'>Country: </span>{selectedMovie?.country}</span> <br/><br/>
-                    <span><span className='grassetto'>Rating: </span>{selectedMovie?.rating}</span> <br/><br/>
-                    <span><span className='grassetto'>Duration: </span>{selectedMovie?.duration}</span> <br/><br/>
-                    <span><span className='grassetto'>Date added on Netflix: </span>{selectedMovie?.date_added}</span> <br/><br/>
+                    <span>{selectedContent?.description}</span> <br/><br/>
+                    <span><span className='grassetto'>Release Date: </span>{selectedContent?.release_year}</span> <br/><br/>
+                    <span><span className='grassetto'>Country: </span>{selectedContent?.country}</span> <br/><br/>
+                    <span><span className='grassetto'>Rating: </span>{selectedContent?.rating}</span> <br/><br/>
+                    <span><span className='grassetto'>Duration: </span>{selectedContent?.duration}</span> <br/><br/>
+                    <span><span className='grassetto'>Date added on Netflix: </span>{selectedContent?.date_added}</span> <br/><br/>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleCloseModal}>
@@ -136,7 +167,6 @@ function ListaFilm() {
                     </Button>
                 </Modal.Footer>
             </Modal>
-
         </div>
     );
 }
